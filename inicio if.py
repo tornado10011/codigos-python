c079d989bc8bce6072f61d4a1875bbe087c16057
tornado10011 <176546458+tornado10011@users.noreply.github.com>
2026-08-17 16:03:36 -0600

    Add files via upload

---
+++ inicio if.py
@@ -0,0 +1,22 @@
+print("verificacion de edad")
+
+edad=int(input("ingrese su edad: \n"))
+
+if edad<18:
+    print("no esta permitida la entrada")
+
+elif edad>60:
+    print("ya es peligrosa su entrada")
+
+else:
+    print("bienvenida")
+
+
+
+    print("verificacion de edad")
+
+edad=int(input("ingrese su edad: \n"))
+if 18<edad<60:
+    print("bienvenido")
+else:
+    print("denegado")
