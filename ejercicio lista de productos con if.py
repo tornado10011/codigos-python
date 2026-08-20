600513b1f054e42a2beef5f9ef04a74797ff22c7
jose <176546458+tornado10011@users.noreply.github.com>
2026-08-19 20:31:01 -0600

    Add files via upload

--- ejercicio lista de productos con if.py
+++ ejercicio lista de productos con if.py
@@ -1,10 +1,25 @@
 lista_de_productos=["leche","pan","queso","naranja"]
+precios=[1500,2000,3500,500]
 
-nuevo_producto=input("indique un producto: ")
+nuevo_producto=input("indique un producto: ")      #le pregunto que indique un producto
 
 if nuevo_producto in lista_de_productos:
-    print("no se puede guardar, ya existe el producto")
+    posicion=lista_de_productos.index(nuevo_producto)    #aqui busco donde esta ubicado ese producto que ya esta en la lista
+    precip=precios[posicion]    #aqui le indico que X precio va a ser el del producto de la posicion X
+    print(f"ya existe el producto {nuevo_producto} con un precio de {precip}")
 else:
-    lista_de_productos.insert(len(lista_de_productos)//2,nuevo_producto)
-    print(lista_de_productos," el producto se añadie correctamente")
+
+    nuev_list=[]   #aqui creo una nueva lista para guardar los nuevos productos
+    nuev_list.append(nuevo_producto)   #se coloca el producto que indico el usuario al final de la nueva lista
+    tuplistnu=tuple(nuev_list)         #se convierte en tupla
+
+    nuevopreci=()
+    nuevo_precio=int(input(f"indique el precio para el articulo {nuevo_producto}: " ))   #aqui indica el precio para el neuvo producto
+    nuevopreci12=list(nuevopreci)
+    nuevopreci12.append(nuevo_precio)  #Aqui se guardara el precio del nuevo articulo
+    nuevopreci=tuple(nuevopreci12)   #aqui la convierto en tupla 
+
+    print(f"para el nuevo articulo; {nuevo_producto} el precio seria de {nuevopreci}\n")
+
+    
     
